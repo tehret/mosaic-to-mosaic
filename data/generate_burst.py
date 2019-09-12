a3ba7c41f2fa5537231693add70bfc9a42fd5f25
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2019-09-12 15:39:45 +0200

    fixing burst generation

--- data/generate_burst.py
+++ data/generate_burst.py
@@ -13,14 +13,9 @@ def sample_noise(size, sigma):
 def sample_transform():
     return [1, (np.random.uniform(0, 1)-0.5)/20, 0, (np.random.uniform(0, 1)-0.5)/20, 1, 0, 0, 0, 1]
 
-def cubic_interpolation(A,B,C,D,x):
-    a,b,c,d = A.size()
-    x = x.view(a,1,c,d).repeat(1,3,1,1)
-    return B + 0.5*x*(C - A + x*(2.*A - 5.*B + 4.*C - D + x*(3.*(B - C) + D - A)))
-
 def rgb2bayer(im):
-    H, W, C = x.size()
-    bayer = torch.zeros((H,W)).cuda()
+    H, W, C = im.shape
+    bayer = np.zeros((H,W))
 
     # Red
     bayer[0::2, 1::2] = im[0::2, 1::2, 0]
@@ -34,6 +29,47 @@ def rgb2bayer(im):
 
     return bayer
 
+def cubic_interpolation(A,B,C,D,x):
+    a,b,c,d = A.size()
+    x = x.view(a,1,c,d).repeat(1,3,1,1)
+    return B + 0.5*x*(C - A + x*(2.*A - 5.*B + 4.*C - D + x*(3.*(B - C) + D - A)))
+
+# Return the img interpolated on the given grid, return also a mask
+def bicubic_interpolation(im, grid):
+    B, C, H, W = im.size()
+
+    x0 = torch.floor(grid[0,0,:,:] - 1).long()
+    y0 = torch.floor(grid[0,1,:,:] - 1).long()
+    x1 = x0 + 1 
+    y1 = y0 + 1 
+    x2 = x0 + 2 
+    y2 = y0 + 2 
+    x3 = x0 + 3 
+    y3 = y0 + 3 
+
+    outsideX = torch.max(torch.gt(-x0, 0), torch.gt(x3, W-1)).repeat((B,C,1,1))
+    outsideY = torch.max(torch.gt(-y0, 0), torch.gt(y3, H-1)).repeat((B,C,1,1))
+
+    mask = torch.Tensor(np.ones((B, C, H, W))).cuda()
+    mask[outsideX] = 0
+    mask[outsideY] = 0
+
+    x0 = x0.clamp(0,W-1)
+    y0 = y0.clamp(0,H-1)
+    x1 = x1.clamp(0,W-1)
+    y1 = y1.clamp(0,H-1)
+    x2 = x2.clamp(0,W-1)
+    y2 = y2.clamp(0,H-1)
+    x3 = x3.clamp(0,W-1)
+    y3 = y3.clamp(0,H-1)
+
+    A = cubic_interpolation(im[:,:,y0,x0],im[:,:,y1,x0],im[:,:,y2,x0],im[:,:,y3,x0], grid[:,1,:,:] - torch.floor(grid[:,1,:,:]))
+    B = cubic_interpolation(im[:,:,y0,x1],im[:,:,y1,x1],im[:,:,y2,x1],im[:,:,y3,x1], grid[:,1,:,:] - torch.floor(grid[:,1,:,:]))
+    C = cubic_interpolation(im[:,:,y0,x2],im[:,:,y1,x2],im[:,:,y2,x2],im[:,:,y3,x2], grid[:,1,:,:] - torch.floor(grid[:,1,:,:]))
+    D = cubic_interpolation(im[:,:,y0,x3],im[:,:,y1,x3],im[:,:,y2,x3],im[:,:,y3,x3], grid[:,1,:,:] - torch.floor(grid[:,1,:,:]))
+    
+    output = cubic_interpolation(A,B,C,D,grid[:,0,:,:] - torch.floor(grid[:,0,:,:]))
+    return output, mask
 # Warp the image according to the transform p (p can be translation up to homography).
 # It also returns a mask of unavailable pixels
 def warp(x, p, scale=1):
@@ -119,7 +155,7 @@ if __name__ == "__main__":
     parser.add_argument('--im', type=str, default='image.png', help='Image used to generate the burst')
     parser.add_argument('--len', type=int, default=1, help='Length of the burst generated')
     parser.add_argument('--sigma', type=int, default=0, help='Noise standard deviation added to the burst')
-    parser.add_argument('--out', type=str, default='burst_%03d.png', help='Output burst')
+    parser.add_argument('--out', type=str, default='burst_%03d.tiff', help='Output burst')
     parser.add_argument('--p', type=str, default='transf.txt', help='File containing the transform used to generate the burst')
     parser.add_argument('--clip', type=bool, default=False, help='Clip the values of the generated burst')
     args = parser.parse_args()
